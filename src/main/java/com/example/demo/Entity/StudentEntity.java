df08e96d86a5421df27ffdd89f3b05e2244dc4e7
bala-amypo <bala@amypo.in>
2025-12-17 05:16:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/Entity/StudentEntity.java
@@ -0,0 +1,14 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Generation;
+import jakarta.persistence.Generate
+
+
+
+
+public class StudentEntity{
+    @Id
+    @GeneratedValue(strategy =)
+}
